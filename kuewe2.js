6b9657cda8abf0e11f04316cb842812b337f8caf
Maciii0809 <124184326+Maciii0809@users.noreply.github.com>
2023-03-13 18:35:27 +0100

    Update kuewe2.js

--- kuewe2.js
+++ kuewe2.js
@@ -1,17 +1,21 @@
-   var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-20vh";
-      }
-      prevScrollpos = currentScrollPos;
+function handleScroll() {
+  var navbar = document.getElementById("navbar"); // Get navbar element inside the function to update it every time the function runs
+  var currentScrollPos = window.pageYOffset;
+  if (window.innerWidth >= 400) { // Check screen size inside the function instead of outside
+    if (prevScrollpos > currentScrollPos) {
+      navbar.style.top = "0";
+    } else {
+      navbar.style.top = "-15vh";
     }
-    
+    prevScrollpos = currentScrollPos;
+  } else {
+    navbar.style.top = "0"; // Set the navbar to be always visible for screens smaller than 400px
+  }
+}
+
+var prevScrollpos = window.pageYOffset;
+window.addEventListener('scroll', handleScroll); // Attach the function to the scroll event for all screen sizes
 
 
-
-
-
+   
 
